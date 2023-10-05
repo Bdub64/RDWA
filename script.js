fe6475e874e585e3491ec1fbf38c91dcd3cd8200
Bdub64 <90049502+Bdub64@users.noreply.github.com>
2023-10-05 15:29:08 -0400

    Hell

---
+++ script.js
@@ -0,0 +1,27 @@
+const gdmOptions = {
+    video: {
+      displaySurface: "window",
+    },
+    audio: {
+      echoCancellation: true,
+      noiseSuppression: true,
+      sampleRate: 44100,
+      suppressLocalAudioPlayback: true,
+    },
+    surfaceSwitching: "include",
+    selfBrowserSurface: "exclude",
+    systemAudio: "exclude",
+  };
+  
+  const videoElem = document.getElementById("video");
+  
+  async function startCapture(displayMediaOptions) {
+  
+    try {
+      videoElem.srcObject =
+        await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  startCapture(gdmOptions)
