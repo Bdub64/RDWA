e3db2385a6f600eb22a46b57565d944ddba17608
Bdub64 <90049502+Bdub64@users.noreply.github.com>
2023-10-13 10:17:46 -0400

    removed extraneous code
    title, but also struggling to find how to capture video on host machine to send to and display on server.

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-const gdmOptions = {
-    video: {
-      displaySurface: "window",
-    },
-    audio: {
-      echoCancellation: true,
-      noiseSuppression: true,
-      sampleRate: 44100,
-      suppressLocalAudioPlayback: true,
-    },
-    surfaceSwitching: "include",
-    selfBrowserSurface: "exclude",
-    systemAudio: "exclude",
-  };
-  
   const videoElem = document.getElementById("video");
   
-  async function startCapture(displayMediaOptions) {
+  async function startCapture() {
   
     try {
       console.log('Attempting C# script run...')
-      videoElem.srcObject = DotNet.invokeMethod('videoClass','getVideo') // should change navigator... to a pull for the c# video feed when next available. pushing to Main for now.
+      videoElem.srcObject =  // should change navigator... to a pull for the c# video feed when next available. pushing to Main for now.
     } catch (err) {
       console.error(err);
     }
   }
-  startCapture(gdmOptions)
+  startCapture()
